ffbc5d94228e98a8bc80aecdb781ca140df6f6c5
Teanoo <532534854@qq.com>
2018-12-03 15:45:08 +0800

    test 2222

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,6 @@ public class Main {
         System.out.println(p.getClass().getInterfaces()[0]);
         p.helloworld();
         System.out.printf("testt");
+        System.out.printf("testttt222222");
     }
 }
